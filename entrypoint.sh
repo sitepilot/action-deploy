1531c1ced82a2067fdfe8ce1d06083264f08a689
Nick Jansen <n.jansen@sitepilot.io>
2021-05-25 14:10:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,11 @@ then
   echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 fi
 
+if [[ -n "$DEPLOY_KEY" ]]
+then
+  mkdir -p ~/.ssh
+  echo "$DEPLOY_KEY" > ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa
+fi
+
 sh -c "$*"
